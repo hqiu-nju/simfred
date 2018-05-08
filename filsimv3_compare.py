d39e66dcbb45e57079d54a9b192aceab28e51fe5
Harry <hqiu678@outlook.com>
2018-05-08 16:50:44 +1000

    fix bug

--- filsimv3_compare.py
+++ filsimv3_compare.py
@@ -252,7 +252,7 @@ for i in xrange(values.number):
         #### snr calculation
         pulse=normmap>0
         snr1 = d.sum()/np.sqrt((d > 0.0).sum())
-        mfactor=mxsnr/snr1 - i*2
+        mfactor=(mxsnr- i*2)/snr1 
         frbconvolvemap=normmap*mfactor
         d2 = frbconvolvemap.flatten()
         #print('nosquare')
